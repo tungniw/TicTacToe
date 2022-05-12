77f5734c4a37485daae0282ae38eb726d9fb1e03
Ben Caplan <bcaplan1994@gmail.com>
2022-05-12 19:04:48 -0400

    minimum viable product

--- UserInterface.cs
+++ UserInterface.cs
@@ -15,6 +15,8 @@ namespace TicTacToe
 
         bool isRunning = true;
 
+        int playersTurn = 0;
+
         
     public void Run()
     {
@@ -25,21 +27,38 @@ namespace TicTacToe
 
     while (isRunning)
     { 
-        
+        if (playersTurn % 2 == 0)
+        {
+            string input = Console.ReadLine();
+            PlaceChoice(input);
+            CreateBoard();
+            playersTurn++; 
+        }
+        else if (playersTurn % 2 != 0)
+        {
+            string input = Console.ReadLine();
+            PlaceChoice2(input);
+            CreateBoard();
+            playersTurn++;
+        }
         
             
         
-        string input = Console.ReadLine();
-        PlaceChoice(input);
-        CreateBoard();
-        CheckWin();
-        
-            
+        // // Console.WriteLine(CheckWin());
+        // string input = Console.ReadLine();
+        // PlaceChoice(input);
+        // PlaceChoice2(input);
+        // CreateBoard();
+        if (CheckWin() == 1)
+        {
+            // Console.Clear();
+            Console.WriteLine("Player O Wins");
+        }   else if (CheckWin() == 2)
+        {
+            // Console.Clear();
+            Console.WriteLine("Player X Wins");
+        }
             // CheckStatus();
-        
-         
-        
-        
     }
     flag = CheckWin();
     }
@@ -111,6 +130,52 @@ namespace TicTacToe
             }
         }
 
+        public string PlaceChoice2(string y)
+        {
+            switch (y)
+            {
+                case "1":
+                    pos[1] = "X";
+                    return pos[1];
+                    break;
+                case "2":
+                    pos[2] = "X";
+                    return pos[2];
+                    break;
+                case "3":
+                    pos[3] = "X";
+                    return pos[3];
+                    break;
+                case "4":
+                    pos[4] = "X";
+                    return pos[4];
+                    break;
+                case "5":
+                    pos[5] = "X";
+                    return pos[5];
+                    break;
+                case "6":
+                    pos[6] = "X";
+                    return pos[6];
+                    break;
+                case "7":
+                    pos[7] = "X";
+                    return pos[7];
+                    break;
+                case "8":
+                    pos[8] = "X";
+                    return pos[8];
+                    break;
+                case "9":
+                    pos[9] = "X";
+                    return pos[9];
+                    break;
+                default:
+                    return "We all lose.";
+                    break;
+            }
+        }
+
         static int CheckWin() // Win checker method ================================================
     {
         if (pos[1] == "O" && pos[2] == "O" && pos[3] == "O") // Horizontal ----------------------------------------
@@ -151,37 +216,37 @@ namespace TicTacToe
 
         if (pos[1] == "X" && pos[2] == "X" && pos[3] == "X") // Horizontal ----------------------------------------
         {
-            return 1;
+            return 2;
         }
         else if (pos[4] == "X" && pos[5] == "X" && pos[6] == "X")
         {
-            return 1;
+            return 2;
         }
         else if (pos[7] == "X" && pos[8] == "X" && pos[9] == "X")
         {
-            return 1;
+            return 2;
         }
 
         else if (pos[1] == "X" && pos[5] == "X" && pos[9] == "X") // Diagonal -----------------------------------------
         {
-            return 1;
+            return 2;
         }
         else if (pos[7] == "X" && pos[5] == "X" && pos[3] == "X")
         {
-            return 1;
+            return 2;
         }
 
         else if (pos[1] == "X" && pos[4] == "X" && pos[7] == "X") // Coloumns ------------------------------------------
         {
-            return 1;
+            return 2;
         }
         else if (pos[2] == "X" && pos[5] == "X" && pos[8] == "X")
         {
-            return 1;
+            return 2;
         }
         else if (pos[3] == "X" && pos[6] == "X" && pos[9] == "X")
         {
-            return 1;
+            return 2;
         }
         else // No winner ----------------------------------------------
         {
